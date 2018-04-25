34588bd602f89dc64f11b5ad5378aded3ea51c0f
Daniel Keen <keendaniel01@gmail.com>
2018-04-25 15:37:29 +0100

    :recycle: refactor: grid creator prints final grid using grid printer class

--- lib/grid_creator.rb
+++ lib/grid_creator.rb
@@ -4,25 +4,9 @@ class GridCreator
   def initialize(width, height)
     @height = height
     @width = width
-
   end
 
-  def vertical_edge_generator
-    @vertical_edges = edge_generator('|', @height)
-  end
-
-  def horizontal_edge_generator
-    @horizontal_edges = edge_generator('-', @width)
-  end
-
-  private
-
-  def edge_generator(edge_type, height_or_width)
-    edges = []
-    return 0 if height_or_width <= 2
-    (height_or_width - 2).times do
-      edges.push(edge_type)
-    end
-    edges
+  def print_grid(printer = GridPrinter.new(EdgeGenerator.new(@height, @width)))
+    print printer.print_full_grid
   end
 end
--- spec/grid_creator_spec.rb
+++ spec/grid_creator_spec.rb
@@ -1,6 +1,7 @@
 require 'grid_creator'
 
 describe GridCreator do
+  let(:grid_printer) { double 'grid printer' }
   subject(:grid_creator) { described_class.new(4, 4) }
 
   describe '#initialize' do
@@ -10,6 +11,13 @@ describe GridCreator do
 
     it 'initializes with a width' do
       expect(grid_creator.width).to eq 4
+    end
+  end
+
+  describe '#print_grid' do
+    it 'prints the grid' do
+      allow(grid_printer).to receive(:print_full_grid) { "\u250C--\u2510\n|  |\n|  |\n\u2514--\u2518"}
+      expect { grid_creator.print_grid(grid_printer) }.to output("\u250C--\u2510\n|  |\n|  |\n\u2514--\u2518").to_stdout
     end
   end
 
